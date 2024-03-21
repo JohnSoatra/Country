6713792c2b60b35fafc8b282e58df196b801547a
soatra <johnsoatra@gmail.com>
2024-03-21 17:04:48 +0700

    Root App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
-import FourOFour from "./pages/404";
+import FourOFour from "@/pages/404";
 
 function App() {
     return (
